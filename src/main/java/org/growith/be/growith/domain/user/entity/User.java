08709d1d2238e643355931abd810ea00bea5fdb6
SeoyeonH <stellastar0921@gmail.com>
2025-09-14 15:08:27 +0900

    이름 바꾸기

---
+++ src/main/java/org/growith/be/growith/domain/user/entity/User.java
@@ -0,0 +1,16 @@
+package org.growith.be.growith.domain.user.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
